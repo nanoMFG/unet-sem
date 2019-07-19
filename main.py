c7354675dd03d42f39d27396f63a81d108a0d749
Joshua Schiller <Joshua_Schiller@Joshuas-MacBook-Pro-2.local>
2019-07-19 17:08:42 -0500

    switched to keras version of multigpu. added argparser for command line input. added a bunch of options for grid searching parameters.

--- main.py
+++ main.py
@@ -1,85 +1,94 @@
 from model import *
 from utils import *
+import argparse
+import sys
 import tensorflow as tf
 from keras.optimizers import *
 from keras.callbacks import ModelCheckpoint, LearningRateScheduler
 from keras.preprocessing.image import ImageDataGenerator
 import keras.backend as K
+from keras.utils import multi_gpu_model
 
-def slice_batch(x, n_gpus, part):
-    sh = K.shape(x)
-    L = K.cast(sh[0] / n_gpus,'int32')
-
-    if part == n_gpus - 1:
-        return x[part*L:]
-    return x[part*L:(part+1)*L]
-
-def to_multi_gpu(model, n_gpus=2):
-    with tf.device('/cpu:0'):
-        x = Input(model.input_shape[1:], name=model.input_names[0])
-
-    towers = []
-
-    for g in range(n_gpus):
-        with tf.device('/gpu:' + str(g)):
-            slice_g = Lambda(slice_batch, lambda shape: shape, arguments={'n_gpus':n_gpus, 'part':g})(x)
-            towers.append(model(slice_g))
-
-    with tf.device('/cpu:0'):
-        merged = Concatenate(axis=0)(towers)
-#         merged = merge(towers, mode='concat', concat_axis=0)
-
-    return Model(input=[x], output=merged)
-
-    ###############################################################
-################ INPUT BLOCK ##################################
-###############################################################
-
-data_gen_args = dict(rotation_range=0.2,
+class TrainUNET:
+    data_gen_args = dict(rotation_range=0.2,
                     width_shift_range=0.05,
                     height_shift_range=0.05,
                     shear_range=0.05,
                     zoom_range=0.05,
                     horizontal_flip=True,
                     fill_mode='nearest')
-n_gpus = 2
-num_epochs = 5
-batch_size =32
-image_mask_paths = [("data/image<%d>.tif"%i,"data/image_mask<%d>.jpg"%i) for i in range(1,41)]
-split = 0.1
-random_crop_size = (256,256)
+    image_mask_paths = [("data/image<%d>.tif"%i,"data/image_mask<%d>.jpg"%i) for i in range(1,41)]
+    def __init__(self,crop=True,augment=True,nepochs=5,batch_size=32,split=0.1,max_crop=False,crop_size=(256,256),input_size=(256,256),ngpu=1,lr=1e-4):
+        num_images = len(image_mask_paths)
+        self.test_paths = image_mask_paths[:int(split*num_images)]
+        self.train_paths = image_mask_paths[int(split*num_images):]
 
-###############################################################
-###############################################################
+        self.crop = crop
+        self.augment = augment
+        self.nepochs = nepochs
+        self.batch_size = batch_size
+        self.split = split
+        self.max_crop = max_crop
+        self.crop_size = crop_size
+        self.input_size = input_size
+        self.ngpu = ngpu
+        self.lr = lr
 
-num_images = len(image_mask_paths)
-test_paths = image_mask_paths[:int(split*num_images)]
-train_paths = image_mask_paths[int(split*num_images):]
+        self.model = unet(input_size=self.input_size+(1,))
+        if self.ngpu > 1:
+            self.model = multi_gpu_model(self.model,gpus=self.ngpu)
+        self.model.compile(optimizer = Adam(lr = self.lr), loss = 'binary_crossentropy', metrics = ['accuracy'])
 
-model = unet(input_size=random_crop_size+(1,))
-if n_gpus > 1:
-    model = to_multi_gpu(model,n_gpus=n_gpus)
-model.compile(optimizer = Adam(lr = 1e-4), loss = 'binary_crossentropy', metrics = ['accuracy'])
-for epoch in range(num_epochs):
-    shuffle(train_paths)
-    for i, img_mask_path in enumerate(train_paths):
-        img, mask = read_data(img_mask_path)
-        aug_imgs, aug_masks = augment_input(img,mask,data_gen_args,random_crop_size=random_crop_size)
-        loss = model.train_on_batch(aug_imgs,aug_masks)
-        print("epoch: %d (%d/%d), %s: %s"%(epoch,i,len(train_paths),model.metrics_names,loss))
+    def train(self):    
+        for epoch in range(self.nepochs):
+            shuffle(self.train_paths)
+            for i, img_mask_path in enumerate(self.train_paths):
+                img, mask = read_data(img_mask_path)
+                aug_imgs, aug_masks = generate_batch(
+                    img,
+                    mask,
+                    batch_size=self.batch_size,
+                    random_crop_size=self.crop_size,
+                    output_size=self.input_size,
+                    crop = self.crop,
+                    augment = self.augment,
+                    aug_dict=data_gen_args,
+                    max_crop = self.max_crop)
+                loss = self.model.train_on_batch(aug_imgs,aug_masks)
+                print("epoch: %d (%d/%d), %s: %s"%(epoch,i,len(self.train_paths),self.model.metrics_names,loss))
 
-    test_loss = []
-    for i, img_mask_path in enumerate(test_paths):
-        img, mask = read_data(img_mask_path)
-        out_imgs,out_masks = augment_input(img,mask,aug_dict=data_gen_args,batch_size=batch_size,random_crop_size=(256,256))
-        #out_imgs = np.zeros((batch_size,random_crop_size[0],random_crop_size[1],1))
-        #out_masks = np.zeros((batch_size,random_crop_size[0],random_crop_size[1],1))
-        #for b in range(batch_size):
-        #    seed = np.random.randint(1e9)
-        #    crop_img, crop_mask = random_crop(img,mask,random_crop_size)
-        #out_imgs[b,...] = image_datagen.random_transform(crop_img,seed=seed)
-        #out_masks[b,...] = image_datagen.random_transform(crop_mask,seed=seed)
+            test_loss = []
+            for i, img_mask_path in enumerate(self.test_paths):
+                img, mask = read_data(img_mask_path)
+                out_imgs,out_masks = generate_batch(
+                    img,
+                    mask,
+                    batch_size=self.batch_size,
+                    random_crop_size=self.crop_size,
+                    output_size=self.input_size,
+                    crop = self.crop,
+                    augment = False,
+                    aug_dict=data_gen_args,
+                    max_crop = self.max_crop)
 
-        test_loss.append(model.test_on_batch(out_imgs,out_masks))
-    test_loss = np.mean(np.array(test_loss),axis=0)
-    print("epoch: %d, %s: %s"%(epoch,model.metrics_names,test_loss))
+                test_loss.append(self.model.test_on_batch(out_imgs,out_masks))
+            test_loss = np.mean(np.array(test_loss),axis=0)
+            print("epoch: %d, %s: %s"%(epoch,self.model.metrics_names,test_loss))
+
+if ___name___ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--crop', action = 'store_true', default = True, help='Constructs batch using random crops.')
+    parser.add_argument('--augment', action = 'store_true', default = True, help='Constructs batch using augmentations.')
+    parser.add_argument('--max_crop', action = 'store_true', default = False, help='Crops using the maximum square size for each image. Crop size is ignored.')
+    parser.add_argument('--crop_size', nargs = 1, default = 256, type = int, help='Size of cropped sample.')
+    parser.add_argument('--input_size', nargs = 1, default = 256, type = int, help='Model input size. Cropped images will be rescaled to this size.')
+    parser.add_argument('--ngpu', nargs = 1, default = 1, type = int, help='Number of GPUs.')
+    parser.add_argument('--nepochs', nargs = 1, default = 5, type = int, help='Number of epochs.')
+    parser.add_argument('--batch_size', nargs = 1, default = 32, type = int, help='Number of samples per batch.')
+    parser.add_argument('--split', nargs = 1, default = 0.1, type = float, help='Fraction of data to use for validation.')
+    parser.add_argument('--lr', nargs = 1, default = 1e-4, type = float, help='Learning rate.')
+
+    kwargs = vars(parser.parse_args(sys.argv))
+
+    run = TrainUNET(**kwargs)
+    run.train()
--- utils.py
+++ utils.py
@@ -1,5 +1,6 @@
 import numpy as np
 import cv2
+from PIL import Image
 from random import shuffle
 from keras.preprocessing.image import ImageDataGenerator
 
@@ -11,21 +12,59 @@ def read_data(img_mask_path):
     mask = cv2.imread(mask_path,cv2.IMREAD_GRAYSCALE)
     mask = mask[...,np.newaxis]
 
-    img = img / 255
-    mask = mask / 255
-    mask[mask > 0.5] = 1
-    mask[mask <= 0.5] = 0
-
     return img, mask
 
-def random_crop(img, mask, random_crop_size):
+def random_crop(img, mask, random_crop_size=(256,256), max_crop=False):
     height, width = img.shape[0], img.shape[1]
-    dy, dx = random_crop_size
+    if max_crop:
+        dx = dy = min(height,width)
+    else:
+        dy, dx = random_crop_size
     x = np.random.randint(0, width - dx + 1)
     y = np.random.randint(0, height - dy + 1)
     return img[y:(y+dy), x:(x+dx), :], mask[y:(y+dy), x:(x+dx), :]
 
-def augment_input(img,mask,aug_dict,batch_size=32,random_crop_size=(256,256)):
+def random_augment(img, mask, output_size, aug_dict):
+    image_datagen = ImageDataGenerator(**aug_dict)
+    seed = np.random.randint(1e9)
+
+    if img.shape != output_size:
+        out_img = Image.fromarray(img[...,0])
+        out_img.resize(output_size)
+        out_mask = Image.fromarray(mask[...,0])
+        out_mask.resize(output_size)
+
+        out_img = np,array(out_img)[...,np.newaxis]
+        out_mask = np,array(out_mask)[...,np.newaxis]
+
+    out_img = image_datagen.random_transform(img,seed=seed)
+    out_mask = image_datagen.random_transform(mask,seed=seed)
+
+    return out_img, out_mask
+
+def generate_batch(img, mask, batch_size=32, random_crop_size=(256,256), output_size=(256,256), crop = True, augment = True, aug_dict = {}, max_crop = False):
+    LX,LY,LZ = img.shape
+
+    out_imgs = np.zeros((batch_size,random_crop_size[0],random_crop_size[1],1))
+    out_masks = np.zeros((batch_size,random_crop_size[0],random_crop_size[1],1))
+    for b in range(batch_size):
+        seed = np.random.randint(1e9)
+        if crop:
+            out_img, out_mask = random_crop(img,mask,random_crop_size)
+        if augment:
+            out_img, out_mask = random_augment(img,mask,output_size,aug_dict)
+
+        out_img = out_img / 255
+        out_mask = out_mask / 255
+        out_mask[mask > 0.5] = 1
+        out_mask[mask <= 0.5] = 0
+
+        out_imgs[b,...] = out_img
+        out_masks[b,...] = out_mask
+
+    return out_imgs, out_masks    
+
+def augment_input(img,mask,aug_dict,batch_size=32,random_crop_size=(256,256),only_crop=False):
     LX,LY,LZ = img.shape
     image_datagen = ImageDataGenerator(**aug_dict)
 
@@ -34,7 +73,11 @@ def augment_input(img,mask,aug_dict,batch_size=32,random_crop_size=(256,256)):
     for b in range(batch_size):
         seed = np.random.randint(1e9)
         crop_img, crop_mask = random_crop(img,mask,random_crop_size)
-        out_imgs[b,...] = image_datagen.random_transform(crop_img,seed=seed)
-        out_masks[b,...] = image_datagen.random_transform(crop_mask,seed=seed)
+        if not only_crop:
+            out_imgs[b,...] = image_datagen.random_transform(crop_img,seed=seed)
+            out_masks[b,...] = image_datagen.random_transform(crop_mask,seed=seed)
+        else:
+            out_imgs[b,...] = crop_img
+            out_masks[b,...] = crop_mask
 
     return out_imgs, out_masks
