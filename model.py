dfda65112537ddd8d7c1edaf14b65a6466054164
Adam Urish <adamurish@gmail.com>
2021-02-01 15:23:46 -0600

    more debugging

--- model.py
+++ model.py
@@ -191,16 +191,18 @@ class TrainUNET:
                         augment = self.augment,
                         aug_dict=self.data_gen_args,
                         max_crop = self.max_crop)
+            print
             for aug_img in aug_imgs:
+                print(aug_img.shape)
                 imgs.append(aug_img)
             for aug_mask in aug_masks:
                 masks.append(aug_mask)
         x = np.array(img)
         y = np.array(mask)
-        self.model.fit(x, y,
-            batch_size=self.batch_size,
-            epochs=self.nepochs,
-            shuffle=True)
+        # self.model.fit(x, y,
+        #     batch_size=self.batch_size,
+        #     epochs=self.nepochs,
+        #     shuffle=True)
         # for epoch in range(self.nepochs):
         #     shuffle(self.train_paths)
         #     for i, img_mask_path in enumerate(self.train_paths):
